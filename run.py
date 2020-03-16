a421abb2ef8a38354e38286061c74d6d29c936f4
youdit <udit.singh98@gmail.com>
2020-03-17 03:31:51 +0530

    comment in run.py

--- run.py
+++ run.py
@@ -6,9 +6,19 @@ from Network import *
 from training import *
 
 model = get_model()
-
+"""5:47 / 12:13￼￼￼￼￼
+The BIGGEST FLAW about Breath of the Wild's DLC5:47 / 12:13￼￼￼￼￼
+The BIGGEST FLAW about Breath of the Wild's DLC
+    input the path of image you want as content on load_image(path) if on the desktop else use load_image_url(name, URL) to get image from net
+    input the path of the image in style image on load_image(path) if on desktop else use load_image_url(name, URL) to get image from net  
+"""
 content_image = load_image('/home/youdit/Pictures/Kushagra.jpeg')
 style_image = load_image_url('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+
+
+"""
+    train will take addition argument optimizer for changing optimizer, learning rate or making custom optimizer 
+"""
 
 image = train(model, content_image, style_image, epochs=100, epochs_per_steps=2)
 
